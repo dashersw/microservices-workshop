141abf505f2cabf118430b2e7e343423b7736b8b
Armagan Amcalar <armagan@amcalar.com>
2018-05-29 02:43:48 +0200

    Use async/await in test

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
 const axios = require('axios')
 
-axios.get('http://localhost:3000/restaurants').then(res => console.log(res.data))
+async function main() {
+  const restaurants = await axios.get('http://localhost:3000/restaurants')
+  console.log(restaurants.data)
 
-axios.post('http://localhost:3000/order', {restaurantId: 1, menuId: 1, address: 'Wall Street 3, 68443'}).then(res => console.log(res.data))
+  const orderRequest = {
+    restaurantId: 1,
+    menuId: 1,
+    address: 'Wall Street 3, 68443'
+  }
+
+  const order = await axios.post('http://localhost:3000/order', orderRequest)
+  console.log(order.data)
+}
+
+main()
